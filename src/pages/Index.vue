8e746cf4be4974a4f03f29f27b3fd78dfe494adb
Equipe Illi <equipeilli@MacBook-Pro-de-Andre.local>
2021-10-13 19:11:32 -0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- card 1 -->
+    <q-card style="min-width:300px;" class="my-card">
+      <q-card-section>
+        <div class="text-overline">Armazenamento</div>
+        <div class="text-h6">Armazenar</div>
+        <q-card-actions>
+          <q-btn color="primary" outline>Acessar</q-btn>
+        </q-card-actions>
+       </q-card-section>
+    </q-card>
+    <!-- card 1 -->
+    <!-- card 2 -->
+    <q-card style="width 100%; min-width:300px;" class="my-card">
+      <q-card-section>
+        <div class="text-overline">Armazenamento</div>
+        <div class="text-h6">Armazenar</div>
+        <q-card-actions>
+          <q-btn color="primary" outline>Acessar</q-btn>
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+    <!-- card 2 -->
+    <!-- card 3 -->
+    <q-card style="min-width:300px;" class="my-card">
+      <q-card-section>
+        <div class="text-overline">Armazenamento</div>
+        <div class="text-h6">Armazenar</div>
+        <q-card-actions>
+          <q-btn color="primary" outline>Acessar</q-btn>
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+    <!-- card 2 -->
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
