e8cb2ceeb58a64b01fd27fbf34d7deb2cf835caf
Equipe Illi <equipeilli@MacBook-Pro-de-Andre.local>
2021-10-15 15:05:48 -0300

    jogar itens na lista feito

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,43 @@
 <template>
   <q-page padding>
     <div class="row q-mb-lg">
-      <q-input class="col" placeholder="Add new task"></q-input>
+      <q-input v-model="newTask" class="col" placeholder="Add new task"></q-input>
       <q-btn 
         color=primary
         size=sm
         label=add
+        @click="addTask"
       ></q-btn>
     </div>
-    <div class="row">
+     <div class="row">
       <q-list class="col" bordered separator>
-       <q-item v-for= "task in tasks" :key = "task" label="task">
+        <q-list-header>Tasks</q-list-header>
+       <q-item v-for= "(task, index) in tasks" v-bind:key = "index" > 
+          <q-item-section>
+          {{task}}  
+          </q-item-section>  
         <q-item-section avatar>
           <q-icon color="green" name="check" />
-        </q-item-section>
-      </q-item>  
+        </q-item-section>   
+     </q-item>  
     </q-list>
     </div>
+    
+     <div class="row">
+      <q-list class="col" bordered separator>
+        <q-list-header>Done</q-list-header>
+       <q-item v-for= "(task, index) in tasks" v-bind:key = "index" > 
+          <q-item-section>
+          {{task}}  
+          </q-item-section>  
+        <q-item-section avatar>
+          <q-icon color="green" name="check" />
+        </q-item-section>   
+     </q-item>  
+    </q-list>
+    
+    </div>
+    
   </q-page>
 </template>
 
@@ -25,11 +46,19 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  data(){
+ data(){
     return {
-      tasks:['wake up','Brush teeth','shower','shower']
+      tasks:[],
+      newTask:''    
     }
     
+  },
+  methods:{
+  addTask(){
+    this.tasks.push(this.newTask)
+    this.newTask=''
   }
-})
+  }
+  })
+  
 </script>
---
+++ src/pages/tabela.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-page padding>
+    <div class="row q-mb-lg">
+      <q-input class="col" placeholder="Add new task"></q-input>
+      <q-btn color=primary size=sm label=add/>
+    </div>
+
+    <div class="row">
+
+      <q-list class="col" bordered separator>
+
+       <q-item v-for= "task in tasks" v-bind:key = "task.id" label="task">
+
+        <q-item-section avatar>
+          <q-icon color="green" name="check" />
+        </q-item-section>
+      </q-item>  
+    </q-list>
+
+    </div>
+    <div class="q-pa-md">
+    <q-table
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+    />
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  data(){
+    return {
+      tasks:[
+        {id:1,task:'wake up'},
+        {id:2,task:'take coffe'},
+        {id:3,task:'brush'},
+      
+      ]
+    }
+    
+  },
+  setup () {
+    return {
+      columns,
+      rows
+    }
+  }
+})
+//tabela
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Registro de entrada',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'pedidoItem', align: 'center', label: 'Pedido Item', field: 'pedidoItem', sortable: true },
+  { name: 'origem', label: 'Retirar em', field: 'origem', sortable: true },
+  { name: 'destino', label: 'Destino', field: 'destino' },
+  { name: 'totalVolumes', label: 'Volumes a conferir', field: 'totalVolumes' },
+  { name: 'volumesLidos', label: 'Volumes Lidos', field: 'volumesLidos' },
+  { name: 'saldo', label: 'Faltam', field: 'saldo', sortable: true, sort: (a, b) => parseInt(a, totalVolumes) - parseInt(b, volumesLidos) },
+  
+]
+
+const rows = [
+  {
+    name: '234345',
+    pedidoItem: '23456789/001',
+    origem: 'B1050101' ,
+    destino: 'B1010101',
+    totalVolumes: 30,
+    volumesLidos: 30,
+    saldo:'0'
+    
+  },
+  {
+    name: '234345',
+    pedidoItem: '23456789/002'
+  }]
+
+
+
+//tabela
+</script>
+
